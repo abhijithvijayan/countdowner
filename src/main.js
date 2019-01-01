aeee9a67789dcf98b6dd7b10323e5a8e2dda9c4f
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-01-01 13:52:25 +0530

    setInterval

--- src/main.js
+++ src/main.js
@@ -11,61 +11,69 @@ var UIController = (function() {
         document.querySelector(pos).textContent = value;
     };
 
+    const getTrueNumber = (num) => (num < 10 ? "0" + num : num);
+
     return {
 
         updateUIElements: (obj) => {
-            update(DOMStrings.daysCount, obj.days);
-            update(DOMStrings.hourCount, obj.hours);
-            update(DOMStrings.minCount, obj.minutes);
-            update(DOMStrings.secCount, obj.seconds);
+            update(DOMStrings.daysCount, getTrueNumber(obj.days));
+            update(DOMStrings.hourCount, getTrueNumber(obj.hours));
+            update(DOMStrings.minCount, getTrueNumber(obj.minutes));
+            update(DOMStrings.secCount, getTrueNumber(obj.seconds));
         }
     }
 
 })();
 
-var globalController = (function(UICtrl) {
+var globalController = (function() {
 
     let getDate = () => {
-        let dateNow, dateFuture, seconds, minutes, hours, days;
+        let dateNow, dateFuture, seconds, minutes, hours, days, cutoff;
 
         dateNow = new Date();
-        console.log(dateNow);
+        // console.log(dateNow);
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
         // new Date(2014, 1, 1) creates a date for 2014-02-01 
-        dateFuture = new Date(new Date().getFullYear() + 1, 0, 1);
-        console.log(dateFuture);
+        dateFuture = new Date(new Date().getFullYear() +1, 0, 1);
 
         seconds = Math.floor((dateFuture - dateNow) / 1000);
+        cutoff = seconds;
         minutes = Math.floor(seconds / 60);
         hours = Math.floor(minutes / 60);
         days = Math.floor(hours / 24);
         hours = hours - (days * 24);
         minutes = minutes - (days * 24 * 60) - (hours * 60);
         seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
-        console.log(days);
-        console.log(hours);
-        console.log(minutes);
+
         return {
             days: days,
             hours: hours,
             minutes: minutes,
-            seconds: seconds
+            seconds: seconds,
+            cutoff: cutoff
         }
 
     };
-
 
     return {
 
         init: () => {
             countdown = getDate();
-            console.log(countdown);
-            UICtrl.updateUIElements(countdown);
+            return countdown.cutoff;
         }
 
     };
 
-})(UIController);
+})();
 
-globalController.init();
+let countdownCall = setInterval(() => {
+    let countDownVal = globalController.init();
+    // stop timer
+    if (countDownVal <= 0) {
+        clearInterval(countdownCall);
+    } 
+    else {
+        UIController.updateUIElements(countdown);
+    }
+}, 1000);
