c101190f0ecd5690e3536d366437920648a9954c
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-01-01 16:58:52 +0530

    code optimising

--- src/main.js
+++ src/main.js
@@ -1,80 +1,45 @@
-var UIController = (function() {
+const DOMStrings = {
+    daysCount: ".timer__content--days",
+    hourCount: ".timer__content--hours",
+    minCount: ".timer__content--minutes",
+    secCount: ".timer__content--seconds",
+    timer: "timer__content--inner"
+};
 
-    const DOMStrings = {
-        daysCount: ".timer__content--days",
-        hourCount: ".timer__content--hours",
-        minCount: ".timer__content--minutes",
-        secCount: ".timer__content--seconds",
+let countDowner = setInterval(() => {
+    let dateNow, dateFuture, seconds, minutes, hours, days, cutoff;
+
+    dateNow = new Date();
+
+    // new Date(2014, 1, 1) creates a date for 2014-02-01 
+    dateFuture = new Date(new Date().getFullYear() +1, 0, 1);
+
+    seconds = Math.floor((dateFuture - dateNow) / 1000);
+    cutoff = seconds;
+    minutes = Math.floor(seconds / 60);
+    hours = Math.floor(minutes / 60);
+    days = Math.floor(hours / 24);
+    hours = hours - (days * 24);
+    minutes = minutes - (days * 24 * 60) - (hours * 60);
+    seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
+
+    if (cutoff <= 0) {
+        clearInterval(countDowner);
+        document.getElementById(timer).textContent = "New Year Wishes!!";
     }
-
-    let update = (pos, value) => {
-        document.querySelector(pos).textContent = value;
-    };
-
-    const getTrueNumber = (num) => (num < 10 ? "0" + num : num);
-
-    return {
-
-        updateUIElements: (obj) => {
-            update(DOMStrings.daysCount, getTrueNumber(obj.days));
-            update(DOMStrings.hourCount, getTrueNumber(obj.hours));
-            update(DOMStrings.minCount, getTrueNumber(obj.minutes));
-            update(DOMStrings.secCount, getTrueNumber(obj.seconds));
-        }
-    }
-
-})();
-
-var globalController = (function() {
-
-    let getDate = () => {
-        let dateNow, dateFuture, seconds, minutes, hours, days, cutoff;
-
-        dateNow = new Date();
-        // console.log(dateNow);
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-
-        // new Date(2014, 1, 1) creates a date for 2014-02-01 
-        dateFuture = new Date(new Date().getFullYear() +1, 0, 1);
-
-        seconds = Math.floor((dateFuture - dateNow) / 1000);
-        cutoff = seconds;
-        minutes = Math.floor(seconds / 60);
-        hours = Math.floor(minutes / 60);
-        days = Math.floor(hours / 24);
-        hours = hours - (days * 24);
-        minutes = minutes - (days * 24 * 60) - (hours * 60);
-        seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
-
-        return {
-            days: days,
-            hours: hours,
-            minutes: minutes,
-            seconds: seconds,
-            cutoff: cutoff
-        }
-
-    };
-
-    return {
-
-        init: () => {
-            countdown = getDate();
-            return countdown.cutoff;
-        }
-
-    };
-
-})();
-
-let countdownCall = setInterval(() => {
-    let countDownVal = globalController.init();
-    // stop timer
-    if (countDownVal <= 0) {
-        clearInterval(countdownCall);
-        document.getElementById('timer__content--inner').textContent = "New Year Wishes!!";
-    } 
     else {
-        UIController.updateUIElements(countdown);
+        update(DOMStrings.daysCount, getTrueNumber(days));
+        update(DOMStrings.hourCount, getTrueNumber(hours));
+        update(DOMStrings.minCount, getTrueNumber(minutes));
+        update(DOMStrings.secCount, getTrueNumber(seconds));
     }
+
 }, 1000);
+
+// content updation
+let update = (pos, value) => {
+    document.querySelector(pos).textContent = value;
+};
+
+// append 0 to the number if < 10
+const getTrueNumber = (num) => (num < 10 ? "0" + num : num);
